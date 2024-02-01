c6d8335c79efd237c1666a1ccc6b586d442307e1
AtomSah <atomsah00@gmail.com>
2024-02-01 08:38:51 +0545

    hehe
    ewfhw

---
+++ wallpaper_frontend/src/pages/components/navbar.jsx
@@ -0,0 +1,13 @@
+
+
+const Navbar = () =>{
+    return(
+        <>
+            <div className={"w-full h-8 text-white justify-center bg-amber-950"}>
+                Navbar
+            </div>
+        </>
+    )
+}
+
+export default Navbar
--- wallpaper_frontend/src/pages/homePages/homePage.jsx
+++ wallpaper_frontend/src/pages/homePages/homePage.jsx
@@ -1,10 +1,12 @@
 import './homepage.css'
+import Navbar from "../components/navbar.jsx";
 
 const HomePage = () =>{
     return(
         <>
+            <Navbar/>
             <div className={"text-5xl text-blue-400"}>
-                ghjk
+                home
             </div>
         </>
     )
